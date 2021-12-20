3e109a6ace98289b62deeaa0e15f04c294fe8441
johnc449 <71931044+johnc449@users.noreply.github.com>
2021-12-20 16:06:13 +0000

    Converting to SQLLITE3

--- Visualisation/populateDB.py
+++ Visualisation/populateDB.py
@@ -1,12 +1,16 @@
 from github import Github   # github api access
 import json                 # for converting a dictionary to a string
-import pymongo              # for mongodb access
+import sqlite3
 import os
 
-#Access Token- Keep this out of the way
-g = Github("--------------------------")
+# Load the faker and its providers
+from faker import Faker     # for anonymising names
+from collections import defaultdict
+faker  = Faker()
+names  = defaultdict(faker.name)
 
-#Let's get the user object and build a data dictionary
+g = Github("_________________________")
+
 usr = g.get_user()
 
 dct = {'user':         names[usr.login].replace(" ", ""), # anonymising
@@ -18,7 +22,50 @@ dct = {'user':         names[usr.login].replace(" ", ""), # anonymising
 
 print ("dictionary is " + json.dumps(dct))
 
+#not cleaning dictionary currently
+#for k, v in dict(dct).items():
+#    if v is None:
+#        del dct[k]
 
+print ("cleaned dictionary is " + json.dumps(dct))
 
+# Establish connection
+conn = sqlite3.connect("Visualisation/data.db")
+conn.execute('''CREATE TABLE MAIN
+         (USER TEXT PRIMARY KEY NOT NULL,
+         FULLNAME          TEXT NOT NULL,
+         LOCATION          TEXT,
+         COMPANY           TEXT,
+         PUBLIC_REPOS      TEXT );''')
+print("Table created successfully");
+#USER,FULLNAME,LOCATION,COMPANY,PUBLIC_REPOS
+conn.execute("INSERT INTO COMPANY (USER,FULLNAME,LOCATION,COMPANY,PUBLIC_REPOS) \ #fix this, wrong format
+      VALUES ("+str(dct["user"])+","+str(dct["fullname"])+","+
+      str(dct["location"])+","+ str(dct["company"])+","+ str(dct["public_repos"])+")")
+
+'''
+# and for each of them we'll get and add a count of the number of repos they have
+fc = usr.followers
+print ("followers: " + str(fc))
+
+# now lets get those followers
+fl = usr.get_followers()
+
+for f in fl:
+    dct = {'user':         names[f.login].replace(" ",""), # anonymising
+           'fullname':     names[f.name], # anonymising
+           'location':     f.location,
+           'company':      f.company,
+           'public_repos': f.public_repos
+           }
+    for k, v in dict(dct).items():
+        if v is None:
+            del dct[k]
+        
+    print("follower: " + json.dumps(dct))
+    conn.execute("INSERT INTO main VALUES (?,?,?,?,?)",
+     [dct["user"], dct["fullname"], dct["location"],dct["company"],dct["public_repos"]]) 
+
+'''
 
 
