23ac1d03e78038fe8d607ce5e7e6168e9db083b7
johnc449 <71931044+johnc449@users.noreply.github.com>
2022-01-03 20:15:57 +0000

    final touches

--- Visualisation/displayData.py
+++ Visualisation/displayData.py
@@ -1,6 +1,6 @@
 
-from pyvis.network import Network
-import re
+from pyvis.network import Network #for visuallising networks
+import re    #for validat
 import sqlite3
 import os
 colours = ["#000000","#00FF00","#0000FF","#FF0000","#01FFFE","#FFA6FE","#FFDB66","#006401","#010067",
@@ -9,15 +9,15 @@ colours = ["#000000","#00FF00","#0000FF","#FF0000","#01FFFE","#FFA6FE","#FFDB66"
 "#263400","#BDD393","#00B917","#9E008E","#001544","#C28C9F","#FF74A3","#01D0FF","#004754","#E56FFE",
 "#788231","#0E4CA1","#91D0CB","#BE9970","#968AE8","#BB8800","#43002C","#DEFF74","#00FFC6","#FFE502"
 "#620E00","#008F9C","#98FF52","#7544B1","#B500FF","#00FF78","#FF6E41","#005F39","#6B6882","#5FAD4E",
-"#A75740","#A5FFD2","#FFB167","#009BFF","#E85EBE"]
-conn = sqlite3.connect('Visualisation/data.db')
-colourDict = {}
-net = Network(notebook=True)
-cursor = conn.execute("SELECT LABEL,COUNT,NEIGHBOURSBOOL from MAIN")
+"#A75740","#A5FFD2","#FFB167","#009BFF","#E85EBE"] #selection of high contrast colours 
+conn = sqlite3.connect('Visualisation/data.db')    #db address
+colourDict = {}    #dict for giving each node a colour
+net = Network(notebook=True)    
+cursor = conn.execute("SELECT LABEL,COUNT,NEIGHBOURSBOOL from MAIN") #fecthes everything in main table
 i = 0
 for row in cursor:
-    net.add_node(row[0],size=row[1]+10,title = "Label used "+str(row[1])+" times.")
-    colourDict[row[0]] = colours[i]
+    net.add_node(row[0],size=row[1]+10,title = "Label used "+str(row[1])+" times.") #creates nodes
+    colourDict[row[0]] = colours[i%len(colours)]   #gives each node an edge colour
     i += 1
 cursor = conn.execute("SELECT LABEL,COUNT,NEIGHBOURSBOOL from MAIN")
 for row in cursor:
@@ -25,7 +25,7 @@ for row in cursor:
         edgeCursor = conn.execute("SELECT LABEL,COUNT from {currentNode}".format(currentNode=re.sub("[^0-9a-zA-Z]+", "_",row[0])))
         for edgeRow in edgeCursor:
             if row[1] > edgeRow[1]:
-              edgeColour = colourDict[row[0]]
+              edgeColour = colourDict[row[0]]    #creates edges and uses colour from bigger node
             else:
               edgeColour = colourDict[edgeRow[0]]
             net.add_edge(row[0],edgeRow[0],width = edgeRow[1]*5,title = "Labels used together "+str(edgeRow[1])+" times.",
@@ -59,5 +59,3 @@ net.set_options('''var options = {
 net.show("Visualisation/Output.html")
 conn.close()
 os.system("start Visualisation/Output.html")
-
-
--- Visualisation/populateDB.py
+++ Visualisation/populateDB.py
@@ -1,39 +1,41 @@
 from github import Github   # github api access
 import json                 # for converting a dictionary to a string
-import sqlite3
-import re
-import os
-from collections import defaultdict
-g = Github("-------------------------------------------")
-repoAddress = input("What repo would you like to get? ")
+import sqlite3         #for database
+import re       #for formatting labels
+import os        #for working with files
+from collections import defaultdict     #similar to dict but allows us to use default values
+g = Github("_______________________") #Put GitHub Key here
+repoAddress = input("What repo would you like to get? ")    
 repo =g.get_repo(repoAddress)
 issues = repo.get_issues()
-labelCountDct = defaultdict(lambda: 0, {})
-labelMixMatrix = defaultdict(lambda: defaultdict(lambda: 0, {}) , {}) 
-for issue in issues:
+labelCountDct = defaultdict(lambda: 0, {})  #dict for number of usuages for each label
+labelMixMatrix = defaultdict(lambda: defaultdict(lambda: 0, {}) , {})  #dict for labels which are used at same time
+for issue in issues:        #iterates over issues
        print(issue.get_labels().totalCount)
        if(issue.get_labels().totalCount == 0):
-           labelCountDct["unlabeled"] += 1
+           labelCountDct["unlabeled"] += 1    #count unlabelled issues
        for label in issue.get_labels():   
               if issue.get_labels().totalCount>1:
-                    for subLabel in issue.get_labels():
-                        if subLabel != label:
+                    for subLabel in issue.get_labels():  #counts number of times labels are used 
+                        if subLabel != label:        #not the best code but does the job
                             labelMixMatrix[label.name][subLabel.name] += 1
               labelCountDct[label.name] += 1
          
 
-print ("dictionary is " + json.dumps(labelCountDct)+"\n"+json.dumps(labelMixMatrix))
+print ("dictionary is " + json.dumps(labelCountDct)+"\n"+json.dumps(labelMixMatrix)) #outputs dictionarys
 
-filePath = "Visualisation/data.db"
+filePath = "Visualisation/data.db"  #put address of database here   
 #Wipes old db if there is one currently at address
 if os.path.exists(filePath):
     os.remove(filePath)
 # Establish connection
 conn = sqlite3.connect(filePath,isolation_level=None)
+#creates main tables
 conn.execute('''CREATE TABLE MAIN
          (LABEL TEXT PRIMARY KEY NOT NULL,
          COUNT          INTEGER NOT NULL,
          NEIGHBOURSBOOL INTEGER NOT NULL);''')
+#fills main table
 for key in labelCountDct:
     conn.execute('INSERT INTO MAIN (LABEL,COUNT,NEIGHBOURSBOOL)\
             VALUES (:label, :count,:neighboursbool)', {"label":key,"count":labelCountDct[key],
